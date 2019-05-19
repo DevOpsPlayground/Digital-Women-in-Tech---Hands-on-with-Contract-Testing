37808c2d573f42ed7b99767275a28e46b20c3d10
samernaqvi <samer.naqvi@ecs-digital.co.uk>
2019-05-20 00:59:46 +0200

    Update BusStopContractTest.java

--- verifyer/src/test/java/se/ff/bsv/BusStopContractTest.java
+++ verifyer/src/test/java/se/ff/bsv/BusStopContractTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RunWith(PactRunner.class) // Say JUnit to run tests with custom Runner
 @Provider("BusService") // Set up name of tested provider
-@PactFolder("pact.rootDir","../pacts") // Point where to find pacts (See also section Pacts source in documentation)
+@PactFolder("./target/pacts") // Point where to find pacts (See also section Pacts source in documentation)
 
 public class BusStopContractTest {
 
